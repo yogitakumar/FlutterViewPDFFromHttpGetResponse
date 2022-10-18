64f15d3d734e54edf6f9d888cfa647743ddab5e1
yogitakumar <helloyogita@gmail.com>
2022-10-18 08:56:14 +0100

    pdf reader

--- lib/ApiServiceProvider.dart
+++ lib/ApiServiceProvider.dart
@@ -3,7 +3,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
 
 class ApiServiceProvider {
-  static final String BASE_URL = "https://www.africau.edu/images/default/sample.pdf";
+  static final String BASE_URL = "https://www.learningcontainer.com/wp-content/uploads/2019/09/sample-pdf-file.pdf";
 
   static Future<String> loadPDF() async {
     var response = await http.get(BASE_URL);
